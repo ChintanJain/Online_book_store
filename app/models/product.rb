21534df989ea1bf8986e339ea90b4a371e5ebe6a
Chintan Jain <chintan_jain21@yahoo.com>
2011-07-23 20:53:13 -0400

    Added migration for the test data. Added CSS file for the index file. Added validation for the title field.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
+  validates_length_of :title, :minimum => 10
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
   validates_format_of :image_url, 
-                      :with => %r{\.(gid|jpg|png)$}i,
+                      :with => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for GIF, JPG, or PNG image.'
             
   protected
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110722042453) do
+ActiveRecord::Schema.define(:version => 20110724002945) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
